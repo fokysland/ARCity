82cd45c8dd7239c9f0d59da8f710c42ccc08c7a1
fokysland <fokysland@yandex.ru>
2020-01-10 14:10:34 +0300

    feat: react-native-maps integration

--- app/screens/Main/screens/Map/Map.screen.js
+++ app/screens/Main/screens/Map/Map.screen.js
@@ -1,8 +1,24 @@
 import React from 'react';
-import {Text} from 'react-native';
+import MapView from 'react-native-maps';
+import {StyleSheet, View, Text} from 'react-native';
 
-const MapScreen = () => {
-  return <Text>Hey</Text>;
-};
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    height: 400,
+    width: 400,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+});
+
+const MapScreen = () => (
+  <View style={styles.container}>
+    <MapView style={styles.map} />
+  </View>
+);
 
 export default MapScreen;

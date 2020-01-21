ae74f4c7cbc85191f42646cde7b0c158db9eea81
fokysland <fokysland@yandex.ru>
2020-01-21 07:57:41 +0300

    fix: dialog screen little fixes

--- app/screens/Main/screens/Dialog/Components/MessageFrame/MessageFrame.screen.js
+++ app/screens/Main/screens/Dialog/Components/MessageFrame/MessageFrame.screen.js
@@ -1,32 +1,27 @@
-import React from "react";
+import React from 'react';
 
-import MessageFrameStyles from "./MessageFrame.styles";
-import { Header } from "_components";
-import { View } from 'react-native';
+import MessageFrameStyles from './MessageFrame.styles';
+import {Header} from '_components';
+import {View} from 'react-native';
 
-
-const MessageFrame = ({ isMy, isLast, msgContent }) => {
-
-
-    let broderType = () => {
-        if (isLast) {
-            if (isMy) {
-                return(MessageFrameStyles.myLastMessage);
-            }
-            else {
-                return(MessageFrameStyles.someonesLastMessage);
-            }
-        }
-        else {
-            return(MessageFrameStyles.notLastMessage);
-        }
+const MessageFrame = ({isMy, isLast, msgContent}) => {
+  let borderType = () => {
+    if (isLast) {
+      if (isMy) {
+        return MessageFrameStyles.myLastMessage;
+      } else {
+        return MessageFrameStyles.someonesLastMessage;
+      }
+    } else {
+      return MessageFrameStyles.notLastMessage;
     }
-    
-    return (
-        <View style={[MessageFrameStyles.messageFrameBackground, broderType()]}>
-            <Header text={msgContent.text} style={MessageFrameStyles.msgText} />
-        </View>
-    );
-}
+  };
+
+  return (
+    <View style={[MessageFrameStyles.messageFrameBackground, borderType()]}>
+      <Header text={msgContent.text} style={MessageFrameStyles.msgText} />
+    </View>
+  );
+};
 
 export default MessageFrame;
--- app/screens/Main/screens/Dialog/Dialog.styles.js
+++ app/screens/Main/screens/Dialog/Dialog.styles.js
@@ -1,30 +1,29 @@
-import { StyleSheet } from 'react-native';
+import {StyleSheet} from 'react-native';
 
-import { Helpers, Typography } from '_styles';
+import {Helpers, Typography} from '_styles/index';
 
 const style = StyleSheet.create({
-    mainContainer: {
-        height: "100%",
-    },
-    flatContainer: {
-        marginTop: "auto",
-    },
-    flatlist: {
-        marginTop: "auto",
-    },
-    clipIcon: {
-        backgroundColor: "#FFF",
-        padding: Helpers.scaleSize(16),
-    },
-    messageInput: {
-        backgroundColor: "#FFF",
-        flex: 1,
-    },
-    inputContainer: {
-        display: "flex",
-        flexDirection: "row",
-    },
-
+  mainContainer: {
+    height: '100%',
+  },
+  flatContainer: {
+    marginTop: 'auto',
+  },
+  flatlist: {
+    marginTop: 'auto',
+  },
+  clipIcon: {
+    backgroundColor: '#FFF',
+    padding: Helpers.scaleSize(16),
+  },
+  messageInput: {
+    backgroundColor: '#FFF',
+    flex: 1,
+  },
+  inputContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+  },
 });
 
 export default style;
